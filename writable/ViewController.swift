148dab33da1fe893a8b859b1dc12127602d40ce1
phuriungung <puriung08@gmail.com>
2023-08-04 21:25:07 +0700

    discovered that bezier path by is just a lie, small linewidth just make illusion that the line is smoother

--- writable/ViewController.swift
+++ writable/ViewController.swift
@@ -1,19 +1,33 @@
 import UIKit
 
+class CustomLineWidthBezierPath: UIBezierPath {
+    var customLineWidth: CGFloat = 1.0
+    
+    override func stroke() {
+        setLineProperties()
+        super.stroke()
+    }
+    
+    private func setLineProperties() {
+        lineWidth = customLineWidth
+        // You can set other line properties here if needed
+    }
+}
+
 class SmoothCanvas: UIView {
+    var lines = [[CGPoint]]()
 
     override func draw(_ rect: CGRect) {
         super.draw(rect)
-
+        
         guard let context = UIGraphicsGetCurrentContext() else { return }
-
+        
         context.setStrokeColor(UIColor.red.cgColor)
-        context.setLineWidth(1000)
         context.setLineCap(.round) // Use round line cap for smoother edges
         
         lines.forEach { line in
             if line.count > 1 {
-                let path = UIBezierPath()
+                let path = CustomLineWidthBezierPath()
                 path.move(to: line[0])
                 
                 for i in 1..<line.count {
@@ -21,13 +35,12 @@ class SmoothCanvas: UIView {
                     path.addLine(to: endPoint)
                 }
                 
+                path.customLineWidth = 10 // Set the custom line width
                 path.stroke()
             }
         }
     }
-
-    var lines = [[CGPoint]]()
-
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         lines.append([CGPoint]())
     }
@@ -36,12 +49,11 @@ class SmoothCanvas: UIView {
         handleTouches(touches)
     }
 
-    // Additional method to handle Pencil touches
-    func handlePencilTouches(_ touches: Set<UITouch>) {
-        guard let touch = touches.first else { return }
-
-        if touch.type == .pencil {
-            let point = touch.location(in: self)
+    func handleTouches(_ touches: Set<UITouch>) {
+        if touches.contains(where: { $0.type == .pencil }) {
+            handlePencilTouches(touches)
+        } else {
+            guard let point = touches.first?.location(in: nil) else { return }
 
             guard var lastLine = lines.popLast() else { return }
             lastLine.append(point)
@@ -50,13 +62,12 @@ class SmoothCanvas: UIView {
             setNeedsDisplay()
         }
     }
+    
+    func handlePencilTouches(_ touches: Set<UITouch>) {
+        guard let touch = touches.first else { return }
 
-    // Combining touch and Apple Pencil handling
-    func handleTouches(_ touches: Set<UITouch>) {
-        if touches.contains(where: { $0.type == .pencil }) {
-            handlePencilTouches(touches)
-        } else {
-            guard let point = touches.first?.location(in: nil) else { return }
+        if touch.type == .pencil {
+            let point = touch.location(in: self)
 
             guard var lastLine = lines.popLast() else { return }
             lastLine.append(point)
