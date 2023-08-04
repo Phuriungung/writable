16dabf017cff063e01b327bca743296fb5fee15a
phuriungung <puriung08@gmail.com>
2023-08-04 20:50:41 +0700

    smooth line by chatgpt but can't change line width

--- writable/ViewController.swift
+++ writable/ViewController.swift
@@ -1,78 +1,78 @@
 import UIKit
 
-class Canvas: UIView {
-    
+class SmoothCanvas: UIView {
+
     override func draw(_ rect: CGRect) {
-        // custom drawing
         super.draw(rect)
-        
+
         guard let context = UIGraphicsGetCurrentContext() else { return }
-        
+
         context.setStrokeColor(UIColor.red.cgColor)
-        context.setLineWidth(10)
-        context.setLineCap(.butt)
+        context.setLineWidth(1000)
+        context.setLineCap(.round) // Use round line cap for smoother edges
         
-        lines.forEach { (line) in
-            for (i, p) in line.enumerated() {
-                if i == 0 {
-                    context.move(to: p)
-                } else {
-                    context.addLine(to: p)
+        lines.forEach { line in
+            if line.count > 1 {
+                let path = UIBezierPath()
+                path.move(to: line[0])
+                
+                for i in 1..<line.count {
+                    let endPoint = line[i]
+                    path.addLine(to: endPoint)
                 }
+                
+                path.stroke()
             }
         }
-        
-        context.strokePath()
     }
-    
+
     var lines = [[CGPoint]]()
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         lines.append([CGPoint]())
     }
-    
+
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         handleTouches(touches)
     }
-    
+
     // Additional method to handle Pencil touches
     func handlePencilTouches(_ touches: Set<UITouch>) {
         guard let touch = touches.first else { return }
-        
-        // Only accept touches from the Apple Pencil
+
         if touch.type == .pencil {
             let point = touch.location(in: self)
-            
+
             guard var lastLine = lines.popLast() else { return }
             lastLine.append(point)
             lines.append(lastLine)
-            
+
             setNeedsDisplay()
         }
     }
-    
+
     // Combining touch and Apple Pencil handling
     func handleTouches(_ touches: Set<UITouch>) {
         if touches.contains(where: { $0.type == .pencil }) {
             handlePencilTouches(touches)
         } else {
             guard let point = touches.first?.location(in: nil) else { return }
-            
+
             guard var lastLine = lines.popLast() else { return }
             lastLine.append(point)
             lines.append(lastLine)
-            
+
             setNeedsDisplay()
         }
     }
 }
 
 class ViewController: UIViewController {
-    let canvas = Canvas()
-    
+    let canvas = SmoothCanvas()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         view.addSubview(canvas)
         canvas.backgroundColor = .white
         canvas.frame = view.frame
