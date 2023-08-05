ab990cc99a4b0ae2d7528b3864176a0a09d2960a
phuriungung <puriung08@gmail.com>
2023-08-05 14:27:30 +0700

    implement quad curve, but not satisfied

--- writable/ViewController.swift
+++ writable/ViewController.swift
@@ -3,11 +3,13 @@ import UIKit
 class Canvas: UIView {
     var bezier = UIBezierPath()
     var strokeColor = UIColor.black
-    var strokeWidth: CGFloat = 2.0
+    var strokeWidth: CGFloat = 10.0
     
     override func draw(_ rect: CGRect) {
         strokeColor.setStroke()
         bezier.lineWidth = strokeWidth
+        bezier.lineCapStyle = .round
+        bezier.lineJoinStyle = .round
         bezier.stroke()
     }
     
@@ -21,7 +23,10 @@ class Canvas: UIView {
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
-        bezier.addLine(to: location)
+        
+        let midPoint = CGPoint(x: (bezier.currentPoint.x + location.x) / 2, y: (bezier.currentPoint.y + location.y) / 2)
+        bezier.addQuadCurve(to: location, controlPoint: midPoint)
+        
         setNeedsDisplay()
     }
 }
