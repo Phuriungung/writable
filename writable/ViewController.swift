c28e420dd72b91b3cc9667e48cbb532c74b82247
phuriungung <puriung08@gmail.com>
2023-08-05 23:37:53 +0700

    hermite, but not perfect

--- writable/ViewController.swift
+++ writable/ViewController.swift
@@ -5,97 +5,158 @@
 //  Created by Nick Dalton on 2/17/17.
 //
 
+import Foundation
 import UIKit
 
+class CanvasView: UIView {
 
-class QuadBezierCurveDrawView: BezierPathDrawView {
+var points: [CGPoint]?
+var path: UIBezierPath?
+var pathLayer: CAShapeLayer!
 
-    var points = [CGPoint]()
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-        
-        guard let touch = touches.first, shouldUseTouchEventForDrawing(touch) else {
-            return
-        }
-        
-        let point = touch.preciseLocation(in: self)
-        self.points.removeAll()
-        self.points.append(point)
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesMoved(touches, with: event)
-        
-        guard let touch = touches.first, shouldUseTouchEventForDrawing(touch) else {
-            return
-        }
-        
-        let point = touch.preciseLocation(in: self)
-        self.points.append(point)
-        
-        if self.points.count == 4 {
-            let middlePoint = CGPoint(x: (self.points[1].x + self.points[3].x) / 2.0,
-                                      y: (self.points[1].y + self.points[3].y) / 2.0)
-            
-            currentPath()?.move(to: self.points[0])
-            currentPath()?.addQuadCurve(to: middlePoint, controlPoint: self.points[1])
-            
-            // replace points and get ready to handle the next segment
-            let lastPoint = self.points[3]
-            self.points.removeAll()
-            self.points.append(middlePoint)
-            self.points.append(lastPoint)
-            
-            // This is not an optimal refresh algorithm. But it works well for short paths. And it's better than full screen refresh.
-            let refreshRect = currentPath()?.bounds.insetBy(dx: -self.lineWidth, dy: -self.lineWidth) ?? self.bounds
-            setNeedsDisplay(refreshRect)
-        }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first, shouldUseTouchEventForDrawing(touch) else {
-            return
-        }
-        
-        // Draw remaining points
-                if self.points.count == 1 {    // only one point acquired = user tapped on the screen
-                    // draw "point"
-                    currentPath()?.lineWidth = self.lineWidth / 2.0
-                    currentPath()?.addArc(withCenter: self.points[0], radius: lineWidth / 4.0, startAngle: 0, endAngle: .pi * 2.0, clockwise: true)
-                    setNeedsDisplay(CGRect(x: self.points[0].x - self.lineWidth, y: self.points[0].y - self.lineWidth,
-                                           width: self.lineWidth * 2.0, height: self.lineWidth * 2.0))
-
-                } else if self.points.count == 2 {
-                    currentPath()?.move(to: self.points[0])
-                    currentPath()?.addLine(to: self.points[1])
-                    
-                    // Create a CGRect from the two points
-                    let rect = CGRect(x: min(self.points[0].x, self.points[1].x) - self.lineWidth,
-                                      y: min(self.points[0].y, self.points[1].y) - self.lineWidth,
-                                      width: abs(self.points[1].x - self.points[0].x) + 2 * self.lineWidth,
-                                      height: abs(self.points[1].y - self.points[0].y) + 2 * self.lineWidth)
-                    setNeedsDisplay(rect)
-                    
-                } else if self.points.count == 3 {
-                    currentPath()?.move(to: self.points[0])
-                    currentPath()?.addQuadCurve(to: self.points[2], controlPoint: self.points[1])
-                    
-                    // Create a CGRect from the three points
-                    let minX = min(self.points[0].x, self.points[1].x, self.points[2].x) - self.lineWidth
-                    let minY = min(self.points[0].y, self.points[1].y, self.points[2].y) - self.lineWidth
-                    let maxX = max(self.points[0].x, self.points[1].x, self.points[2].x) + self.lineWidth
-                    let maxY = max(self.points[0].y, self.points[1].y, self.points[2].y) + self.lineWidth
-                    let rect = CGRect(x: minX, y: minY, width: maxX - minX, height: maxY - minY)
-                    setNeedsDisplay(rect)
-                }
-                
-                self.points.removeAll()
-                
-                super.touchesEnded(touches, with: event)
-    }
+override func layoutSubviews() {
 
 }
+
+override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    pathLayer = CAShapeLayer()
+    pathLayer.fillColor = UIColor.clear.cgColor
+    pathLayer.strokeColor = UIColor.red.cgColor
+    pathLayer.lineWidth = 10
+    pathLayer.lineJoin = CAShapeLayerLineJoin.round
+    pathLayer.lineCap = CAShapeLayerLineCap.round
+    self.layer.addSublayer(pathLayer)
+
+    if let touch = touches.first {
+
+        points = [touch.location(in: self)]
+    }
+}
+
+override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    if let touch = touches.first {
+
+        if #available(iOS 9.0, *) {
+
+            if let coalescedTouches = event?.coalescedTouches(for: touch) {
+
+                points? += coalescedTouches.map { $0.location(in: self) }
+            }
+            else {
+
+                points?.append(touch.location(in: self))
+            }
+
+            if let predictedTouches = event?.predictedTouches(for: touch) {
+
+                let predictedPoints = predictedTouches.map { $0.location(in: self) }
+                pathLayer.path = UIBezierPath.interpolateHermiteFor(points: points! + predictedPoints, closed: false).cgPath
+            }
+            else {
+
+                pathLayer.path = UIBezierPath.interpolateHermiteFor(points: points!, closed: false).cgPath
+            }
+        }
+        else {
+
+            points?.append(touch.location(in: self))
+            pathLayer.path = UIBezierPath.interpolateHermiteFor(points: points!, closed: false).cgPath
+        }
+    }
+}
+
+override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+    pathLayer.path = UIBezierPath.interpolateHermiteFor(points: points!, closed: false).cgPath
+    points?.removeAll()
+}
+}
+
+
+extension UIBezierPath {
+
+static func interpolateHermiteFor(points: [CGPoint], closed: Bool = false) -> UIBezierPath {
+    guard points.count >= 2 else {
+        return UIBezierPath()
+    }
+
+    if points.count == 2 {
+        let bezierPath = UIBezierPath()
+        bezierPath.move(to: points[0])
+        bezierPath.addLine(to: points[1])
+        return bezierPath
+    }
+
+    let nCurves = closed ? points.count : points.count - 1
+
+    let path = UIBezierPath()
+    for i in 0..<nCurves {
+        var curPt = points[i]
+        var prevPt: CGPoint, nextPt: CGPoint, endPt: CGPoint
+        if i == 0 {
+            path.move(to: curPt)
+        }
+
+        var nexti = (i+1)%points.count
+        var previ = (i-1 < 0 ? points.count-1 : i-1)
+
+        prevPt = points[previ]
+        nextPt = points[nexti]
+        endPt = nextPt
+
+        var mx: CGFloat
+        var my: CGFloat
+        if closed || i > 0 {
+            mx  = (nextPt.x - curPt.x) * CGFloat(0.5)
+            mx += (curPt.x - prevPt.x) * CGFloat(0.5)
+            my  = (nextPt.y - curPt.y) * CGFloat(0.5)
+            my += (curPt.y - prevPt.y) * CGFloat(0.5)
+        }
+        else {
+            mx = (nextPt.x - curPt.x) * CGFloat(0.5)
+            my = (nextPt.y - curPt.y) * CGFloat(0.5)
+        }
+
+        var ctrlPt1 = CGPoint.zero
+        ctrlPt1.x = curPt.x + mx / CGFloat(3.0)
+        ctrlPt1.y = curPt.y + my / CGFloat(3.0)
+
+        curPt = points[nexti]
+
+        nexti = (nexti + 1) % points.count
+        previ = i;
+
+        prevPt = points[previ]
+        nextPt = points[nexti]
+
+        if closed || i < nCurves-1 {
+            mx  = (nextPt.x - curPt.x) * CGFloat(0.5)
+            mx += (curPt.x - prevPt.x) * CGFloat(0.5)
+            my  = (nextPt.y - curPt.y) * CGFloat(0.5)
+            my += (curPt.y - prevPt.y) * CGFloat(0.5)
+        }
+        else {
+            mx = (curPt.x - prevPt.x) * CGFloat(0.5)
+            my = (curPt.y - prevPt.y) * CGFloat(0.5)
+        }
+
+        var ctrlPt2 = CGPoint.zero
+        ctrlPt2.x = curPt.x - mx / CGFloat(3.0)
+        ctrlPt2.y = curPt.y - my / CGFloat(3.0)
+
+        path.addCurve(to: endPt, controlPoint1:ctrlPt1, controlPoint2:ctrlPt2)
+    }
+
+    if closed {
+        path.close()
+    }
+
+    return path
+}
+}
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -104,8 +165,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // Create an instance of QuadBezierCurveDrawView
-        let drawView = QuadBezierCurveDrawView(frame: view.bounds)
-        drawView.lineWidth = 10.0 // Set desired line width
+        let drawView = CanvasView(frame: view.bounds)
+        // Set desired line width
         view.addSubview(drawView)
     }
 }
